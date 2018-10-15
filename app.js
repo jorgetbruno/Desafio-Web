d40c112960965bb34a91d2a5d72e7111f1223ce9
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2018-10-15 14:01:23 +0000

    Correção para a porta do app.listen, agora é possivel rodar a aplicação fora em um ambiente c9 sem que seja necessário realiar alterações

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+//Dependências utilizadas
 var express = require('express');
 var app = express();
 
+//Variavél para identificarqual porta o express deverá utilizar, em uma VM c9 irá utilizar a porta padrão, já em outros ambientes utilizará a porta 3000
+var PORT = process.env.PORT !== undefined ? process.env.PORT : 3000;
+
+//Rota para home
 app.get('/', function (req, res) {
   res.render('home.ejs');
 });
 
+//Rota para um curso determinado
 app.get('/curso/:idCurso', function (req, res) {
-
   res.render('curso.ejs');
 });
 
+//Rota para páginas inexistentes
 app.get('*', function (req, res) {
   res.send('404 - page not found');
 });
 
-app.listen(process.env.PORT, function () {
+//Inicia um socket UNIX para excutar as conexões
+app.listen(PORT, function () {
   console.log('It works');
 });
